f9ce99e045ccae7f4a5c7dd6340ebd5b3adc3363
ruslan ishmaev <ruslanishmaev03@gmail.com>
2024-03-21 22:29:42 +0300

    Closes #9, #10

--- tg_bot/management/commands/bot.py
+++ tg_bot/management/commands/bot.py
@@ -1,3 +1,4 @@
+import telebot.types
 from django.core.management.base import BaseCommand
 from django.conf import settings
 from tg_bot.models import *
@@ -6,23 +7,24 @@ from tg_bot.models import *
 from telebot import TeleBot
 
 
-# Объявление переменной бота
 bot = TeleBot(settings.TELEGRAM_BOT_API_KEY, threaded=False)
 
 
-# Название класса обязательно - "Command"
 class Command(BaseCommand):
-  	# Используется как описание команды обычно
     help = 'Just a command for launching a Telegram bot.'
 
     def handle(self, *args, **kwargs):
         print("Bot initiation started")
-        bot.enable_save_next_step_handlers(delay=2) # Сохранение обработчиков
-        bot.load_next_step_handlers()		# Загрузка обработчиков
+        bot.enable_save_next_step_handlers(delay=2)  # Сохранение обработчиков
+        bot.load_next_step_handlers()  # Загрузка обработчиков
+        bot.set_my_commands([telebot.types.BotCommand(command="/start",
+                                                      description="Запускает бота и отправляет приветствие"),
+                             telebot.types.BotCommand(command="/help",
+                                                      description="Полезная информация о работе бота")])
         print("Bot initiation completed")
         bot.infinity_polling()
 
-    @bot.message_handler(commands=['start'])
+    @bot.message_handler(commands=['start'])  # handler команды /start, который регистрирует пользователя в бд
     def send_welcome(self):
         tg_user_id = self.from_user.id
         chat_id = self.chat.id
@@ -31,3 +33,7 @@ class Command(BaseCommand):
             tg_user.save()
         bot.send_message(chat_id, f'Привет, {self.from_user.username},\nэто телеграм-бот, в котором ты можешь:'
                                   f' выращивать своего питомца и ухаживать за ним')
+
+    @bot.message_handler(commands=['help'])
+    def help_handler(self):
+        bot.send_message(self.chat.id, "Бот создан командой FTM, текущая версия v0.1")
